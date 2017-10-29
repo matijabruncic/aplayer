8b5c3d74534236322206327fc4d0a4c3d75154f6
Matija Bruncic <matija.bruncic@infobip.com>
2017-10-29 13:39:25 +0100

    fixed web server hanging

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
 import time
-from threading import Thread
-
-from threading import Thread
+from multiprocessing import Process
 
 import am_i_home
 import configurator
@@ -9,17 +7,21 @@ import song_manager
 import stream_a_song
 import video_links_fetcher
 
-thread = Thread(target=configurator.run)
-thread.start()
+server = Process(target=configurator.run)
+server.start()
 
 old_value = am_i_home.check()
-while True:
-    new_value = am_i_home.check()
-    if new_value is False:
-        time.sleep(1)
-    elif new_value != old_value:
-        song_name = song_manager.random_song_name()
-        video_links_fetcher.fetch(song_name)
-        print "You've just got home and I got a song for you: " + song_name
-        stream_a_song.play(song_name)
-    old_value = new_value
+try:
+    while True:
+        new_value = am_i_home.check()
+        if new_value is False:
+            time.sleep(1)
+        elif new_value != old_value:
+            song_name = song_manager.random_song_name()
+            video_links_fetcher.fetch(song_name)
+            print "You've just got home and I got a song for you: " + song_name
+            stream_a_song.play(song_name)
+        old_value = new_value
+except KeyboardInterrupt:
+    server.terminate()
+    server.join()
